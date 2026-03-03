82c920daedd87a3efa7a17792a5a7cc595aaa8df
RonanTardif <67594820+RonanTardif@users.noreply.github.com>
2026-03-03 22:29:21 +0100

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,13 +1,23 @@
 export const WHATSAPP_LINK = "https://chat.whatsapp.com/LTgk0slmdpC9DKPaittyaY?mode=gi_t";
 
-export const ROOMS_API =
-  "https://script.google.com/macros/s/AKfycbwZopPyM1mSWliuEMVI0D24v1GCHqSYEmcmeaJTMwQ9MmxxML4Hxwi63d2UDK06Xsu9uw/exec?path=rooms";
+//export const ROOMS_API =
+//  "https://script.google.com/macros/s/AKfycbwZopPyM1mSWliuEMVI0D24v1GCHqSYEmcmeaJTMwQ9MmxxML4Hxwi63d2UDK06Xsu9uw/exec?path=rooms";
 
-export const PHOTOS_API =
-  "https://script.google.com/macros/s/AKfycbwZopPyM1mSWliuEMVI0D24v1GCHqSYEmcmeaJTMwQ9MmxxML4Hxwi63d2UDK06Xsu9uw/exec/exec?path=data";
+//export const PHOTOS_API =
+// "https://script.google.com/macros/s/AKfycbwZopPyM1mSWliuEMVI0D24v1GCHqSYEmcmeaJTMwQ9MmxxML4Hxwi63d2UDK06Xsu9uw/exec/exec?path=data";
+
+//export const ADMIN_API =
+//  "https://script.google.com/macros/s/AKfycbwZopPyM1mSWliuEMVI0D24v1GCHqSYEmcmeaJTMwQ9MmxxML4Hxwi63d2UDK06Xsu9uw/exec?path=admin";
+
 
 export const PHOTOS_REFRESH_MS = 15000;
 export const QUIZ_STORAGE_KEY = "mariage_quiz_scores_v1";
 
+export const ROOMS_API =
+  "https://script.google.com/macros/s/AKfycbyM1MuhsHRYLwryqZG7kaAKzCCJ55Vc5m54g3VhzCewNqCuvjavtMAaEEajLaSgI8QqsA/exec?path=rooms";
+
+export const PHOTOS_API =
+  "https://script.google.com/macros/s/AKfycbyM1MuhsHRYLwryqZG7kaAKzCCJ55Vc5m54g3VhzCewNqCuvjavtMAaEEajLaSgI8QqsA/exec?path=data";
+
 export const ADMIN_API =
-  "https://script.google.com/macros/s/AKfycbwZopPyM1mSWliuEMVI0D24v1GCHqSYEmcmeaJTMwQ9MmxxML4Hxwi63d2UDK06Xsu9uw/exec?path=admin";
+  "https://script.google.com/macros/s/AKfycbyM1MuhsHRYLwryqZG7kaAKzCCJ55Vc5m54g3VhzCewNqCuvjavtMAaEEajLaSgI8QqsA/exec?path=admin";
